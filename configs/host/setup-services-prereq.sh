d4e3184fa19efc667ae5f797c8125676bef9e148
VladStPetre <vladstefan.petre@gmail.com>
2025-12-10 17:16:54 +0200

    add deploy stacks commands to helper

--- configs/host/setup-services-prereq.sh
+++ configs/host/setup-services-prereq.sh
@@ -3,19 +3,19 @@
 set -e
 
 
-echo "### Create docker network..."
+echo "===  Create docker network  ==="
 docker network create --subnet=172.21.0.0/16 -d overlay hlel
-echo "### Docker network created..."
+echo "===  Docker network created  ==="
 
-echo "### Create docker secrets..."
+echo "===  Create docker secrets  ==="
 printf 'admin' | docker secret create influxdb2-admin-username -
 printf 'vladminmax.2251' | docker secret create influxdb2-admin-password -
 printf 'fmfi2NeHqnW5MzeTt2NA3x2r!DWFD2wc' | docker secret create influxdb2-admin-token -
 printf 'fuUcdStQCAymj5BS7e7FNyxdhR9XM77u' | docker secret create immich_pg_password -
-echo "### Docker secrets created..."
+echo "===  Docker secrets created  ==="
 
 
-echo "### Create docker configs..."
+echo "===  Create docker configs  ==="
 docker config create traefik_yaml_config ./configs/traefik/traefik.yml
 docker config create traefik_config_yaml_config ./configs/traefik/config.yml
 
@@ -30,4 +30,16 @@ docker config create homepage_settings_yaml ./configs/homepage/settings.yaml
 docker config create homepage_services_yaml ./configs/homepage/services.yaml
 docker config create homepage_bookmarks_yaml ./configs/homepage/bookmarks.yaml
 docker config create homepage_widgets_yaml ./configs/homepage/widgets.yaml
-echo "### Docker configs created..."
+echo "===  Docker configs created  ==="
+
+echo "===  Deploy stacks  ==="
+docker stack deploy -c stacks/backup/docker-stack.yaml --prune --resolve-image=changed backup
+docker stack deploy -c stacks/gh-runner/docker-stack.yaml --prune --resolve-image=changed gh-runner
+docker stack deploy -c stacks/ha/docker-stack.yaml --prune --resolve-image=changed ha
+docker stack deploy -c stacks/immich/docker-stack.yaml --prune --resolve-image=changed immich
+docker stack deploy -c stacks/infra/docker-stack.yaml --prune --resolve-image=changed infra
+docker stack deploy -c stacks/kiosk/docker-stack.yaml --prune --resolve-image=changed kiosk
+docker stack deploy -c stacks/media/docker-stack.yaml --prune --resolve-image=changed media
+docker stack deploy -c stacks/monitoring/docker-stack.yaml --prune --resolve-image=changed monitoring
+docker stack deploy -c stacks/utils/docker-stack.yaml --prune --resolve-image=changed utils
+echo "===  Stacks deployed  ==="
